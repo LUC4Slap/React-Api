58008c68e9c2b51a821580e58479da48e93eb031
LUC4Slap <lucaslap27@gmail.com>
2020-04-09 12:47:54 -0400

    first commit

---
+++ games/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Corpo from './components/Corpo';
+
+function App() {
+  return (
+    <div className="App">
+      <Corpo />
+    </div>
+  );
+}
+
+export default App;
---
+++ games/src/components/Corpo.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Corpo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            resposta: []
+        };
+    }
+    async chamaApi() {
+        let dados = await axios.get('http://localhost:8080/games');
+        console.log(dados.data);
+        this.setState({ resposta: dados.data });
+        return dados.data;
+    }
+    componentDidMount() {
+        this.chamaApi();
+    }
+    render() {
+        return (
+            <div>
+                { this.state.resposta.map(m => {
+                    return (
+                        <div className="container" key={m.id}>
+                            <div className="card mt-3">
+                                <div className="card-header">
+                                    {m.title}
+                                </div>
+                                <div className="card-body">
+                                    {m.price}
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+}
